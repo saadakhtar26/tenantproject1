dfac5b3a261010d25450c8b482f4ccb63ebb66cd
Saad Akhtar <saadakhtar2000@gmail.com>
2022-03-23 05:20:34 +0500

    First Day All Done

--- backend/controllers/tenantController.js
+++ backend/controllers/tenantController.js
@@ -3,7 +3,6 @@ const tenant = require('../models/tenantModel')
 
 const dashboard = asyncHandler(async (req, res) => {
     const address = await tenant.find()
-
     res.status(200).json(address)
 })
 
@@ -12,17 +11,20 @@ const addResidency = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please add an address')
     }
-
-    res.status(200).json({message:'Add New Tenant Residency'})
+    const address = await tenant.create({
+        text: req.body.address
+    })
+    res.status(200).json(address)
 })
 
 const delResidency = asyncHandler(async (req, res) => {
+    const address = await tenant.findById(req.params.id)
     if(!req.params.id){
         res.status(400)
         throw new Error('Please add an ID')
     }
-    
-    res.status(200).json({message:`Delete Tenant Residency ID: ${req.params.id}, IF NOT Already`})
+    await address.remove()
+    res.status(200).json({id: req.params.id})
 })
 
 module.exports = {
