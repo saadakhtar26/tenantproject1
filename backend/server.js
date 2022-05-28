fa6c4d6e55b95ea7493d3dff5f6351cd313a3f32
Saad Akhtar <saadakhtar2000@gmail.com>
2022-05-28 21:41:32 +0500

    CORS Enable try 1

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
@@ -7,9 +6,15 @@ const port = process.env.PORT || 5000
 
 connectDB()
 const app = express()
-app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
+
+app.options('/api/tenant/login', function (req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.end();
+});
 
 app.use('/api/tenant', require('./routes/tenantRoutes'))
 app.use('/api/hotel', require('./routes/hotelRoutes'))
