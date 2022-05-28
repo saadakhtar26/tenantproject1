7fa51cf21e4df9cd41fd03398d3dd0b78cad392a
Saad Akhtar <saadakhtar2000@gmail.com>
2022-05-28 17:17:09 +0500

    CORS Enable

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
@@ -6,6 +7,7 @@ const port = process.env.PORT || 5000
 
 connectDB()
 const app = express()
+app.use(cors)
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 
