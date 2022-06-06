0d8545668aff6b81a72b81ec7d3a3a624b2149c4
Saad Akhtar <saadakhtar2000@gmail.com>
2022-06-06 19:17:22 +0500

    inclusion error fix

--- backend/controllers/tenantController.js
+++ backend/controllers/tenantController.js
@@ -75,15 +75,15 @@ const dashboard = asyncHandler(async (req, res) => {
     
     const tenant = await tenantModel.findById(req.user.id,'-_id -__v -password')
     
-    const residence = await residenceModel.find({ 'tenant' : req.user.id, 'isActive' : true }, '_id -__v -tenant')
-    .populate('station', '-_id station_name address')
+    const residence = await residenceModel.findOne({ 'tenant' : req.user.id, 'isActive' : true }, '_id -__v -tenant')
     
     if(residence==null || residence.length==0){
         const stations = await stationModel.find({},'_id station_name')
         res.status(200).json({ "status":"success", "tenant":tenant, "residence":null, "stations":stations })
     }
     else{
-        res.status(200).json({ "status":"success", "tenant":tenant, "residence":residence })
+        const station = await stationModel.findById(residence.station,'-_id station_name')
+        res.status(200).json({ "status":"success", "tenant":tenant, "residence":residence, "station":station })
     }
 })
 
