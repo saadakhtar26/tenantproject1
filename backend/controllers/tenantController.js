f8acf70da3342b097ed06c8dfed1618075030d92
Saad Akhtar <saadakhtar2000@gmail.com>
2022-06-06 12:48:00 +0500

    tenant dashboard residence id response

--- backend/controllers/tenantController.js
+++ backend/controllers/tenantController.js
@@ -75,7 +75,7 @@ const dashboard = asyncHandler(async (req, res) => {
     
     const tenant = await tenantModel.findById(req.user.id,'-_id -__v -password')
     
-    const residence = await residenceModel.find({ 'tenant' : req.user.id, 'isActive' : true }, '-_id -__v -tenant')
+    const residence = await residenceModel.find({ 'tenant' : req.user.id, 'isActive' : true }, '_id -__v -tenant')
     .populate('station', '-_id station_name address')
     
     if(residence==null || residence.length==0){
